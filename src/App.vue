1918f1a8bdb41b59d5363a9ff565114f0bbbfe37
Kalyn Coose <kalyncoose@gmail.com>
2022-07-16 01:43:15 -0500

    Force refresh on back traversal

--- src/App.vue
+++ src/App.vue
@@ -47,5 +47,16 @@ export default {
       return Math.floor(Math.random() * max);
     }
   },
+  mounted() {
+    window.addEventListener( "pageshow", function ( event ) {
+      var historyTraversal = event.persisted || 
+                         ( typeof window.performance != "undefined" && 
+                              window.performance.navigation.type === 2 );
+        if ( historyTraversal ) {
+        // Handle page restore.
+        window.location.reload();
+  }
+});
+  }
 };
 </script>
